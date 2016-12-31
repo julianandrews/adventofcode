9c46dbd57d63a271c4058dd1c3286937e8253fea
Julian Andrews <jandrews@fusionbox.com>
2016-12-31 15:25:02 -0700

    Some refactoring for 14

---
+++ 14.py
@@ -0,0 +1,50 @@
+import collections
+from functools import lru_cache
+import hashlib
+import re
+
+
+TRIPLET_RE = re.compile(r'(.)\1\1')
+QUINT_RE = re.compile(r'(.)\1\1\1\1')
+
+
+@lru_cache(1001)
+def hash_value(seed, i, stretch):
+    value = (seed + str(i))
+    for i in range(2017 if stretch else 1):
+        value = hashlib.md5(value.encode()).hexdigest()
+    return value
+
+
+def get_pad_length(seed, stretch=False):
+    hashes = []
+
+    quints = collections.defaultdict(int)
+    def add_quints(i):
+        for c in QUINT_RE.findall(hash_value(seed, i, stretch)):
+            quints[c] = i
+    for i in range(1001):
+        add_quints(i)
+
+    i = -1
+    while len(hashes) < 64:
+        i += 1
+        digest = hash_value(seed, i, stretch)
+        match = TRIPLET_RE.search(digest)
+        c = match.group(1) if match is not None else None
+        if c is not None and i < quints[c] <= i + 1000:
+            hashes.append(i)
+
+        add_quints(i + 1000)
+
+    return i
+
+
+if __name__ == '__main__':
+    assert get_pad_length('abc') == 22728
+    assert hash_value('abc', 0, True) == 'a107ff634856bb300138cac6568c0f24'
+    assert get_pad_length('abc', stretch=True) == 22551
+    print("All tests passed")
+
+    print(get_pad_length('yjdafjpo'))
+    print(get_pad_length('yjdafjpo', stretch=True))
