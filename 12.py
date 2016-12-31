0a9ffad8d47d5f3f9d537e47c485ea7c44d814ae
Julian Andrews <jandrews@fusionbox.com>
2016-12-31 09:23:10 -0700

    Clean up 12

--- 12.py
+++ 12.py
@@ -1,8 +1,11 @@
+from utils import lines, read_data
+
+
 class AssembunnyRunner(object):
-    def __init__(self, data):
+    def __init__(self, data, initial=None):
         self.registers = {'a': 0, 'b': 0, 'c': 0, 'd': 0}
-        lines = [s.strip() for s in data.strip().split('\n')]
-        self.instructions = [line.split(' ', 1) for line in lines]
+        self.registers.update(initial if initial is not None else {})
+        self.instructions = [line.split(' ', 1) for line in lines(data)]
 
     def parse_value(self, source):
         try:
@@ -10,7 +13,7 @@ class AssembunnyRunner(object):
         except ValueError:
             return self.registers[source]
 
-    def run(self):
+    def __call__(self):
         i = 0
         while i < len(self.instructions):
             inst, args = self.instructions[i]
@@ -28,14 +31,16 @@ class AssembunnyRunner(object):
             i += 1
 
 
-def doit(data):
-    runner = AssembunnyRunner(data)
-    runner.run()
+def get_a_register(data, initial=None):
+    runner = AssembunnyRunner(data, initial)
+    runner()
     return runner.registers['a']
 
 
 if __name__ == '__main__':
-    assert doit(
+    data = read_data(12)
+
+    assert get_a_register(
         """
         cpy 41 a
         inc a
@@ -47,6 +52,5 @@ if __name__ == '__main__':
     ) == 42
     print("All tests passed")
 
-    with open('data/d12.txt') as f:
-        data = f.read()
-    print(doit(data))
+    print(get_a_register(data))
+    print(get_a_register(data, {'c': 1}))
