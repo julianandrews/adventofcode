4c44e8b8fec4750b5b3782c52318524df917925c
Julian Andrews <jandrews@fusionbox.com>
2018-12-03 20:47:47 -0800

    Refactor

--- 2018/problem_03.py
+++ 2018/problem_03.py
@@ -1,36 +1,30 @@
 import collections
+import re
 
 import utils
 
 
-Claim = collections.namedtuple('Claim', ['num', 'x', 'y', 'h', 'w'])
+Claim = collections.namedtuple('Claim', ['id', 'x', 'y', 'w', 'h'])
+CLAIM_RE = re.compile(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)")
 
 
 def parse_claim(s):
-    parts = s.split()
-    num = parts[0][1:]
-    x, y = parts[2][:-1].split(',')
-    w, h = parts[3].split('x')
-    return Claim(int(num), int(x), int(y), int(h), int(w))
+    return Claim(*map(int, CLAIM_RE.match(s.strip()).groups()))
 
 
 def claim_points(claim):
-    points = set()
-    for x in range(claim.x, claim.x + claim.w):
-        for y in range(claim.y, claim.y + claim.h):
-            points.add((x, y))
-    return points
+    return {(x, y)
+            for x in range(claim.x, claim.x + claim.w)
+            for y in range(claim.y, claim.y + claim.h)}
 
 
 def get_overlap(claims):
+    all_points = set()
     overlap = set()
-    points = set()
     for claim in claims:
-        ps = claim_points(claim)
-        for p in ps:
-            if p in points:
-                overlap.add(p)
-            points.add(p)
+        new_points = claim_points(claim)
+        overlap |= (new_points & all_points)
+        all_points |= new_points
     return overlap
 
 
@@ -43,10 +37,13 @@ def p2(data):
     claims = [parse_claim(s) for s in data]
     overlap = get_overlap(claims)
 
-    for claim in claims:
-        ps = claim_points(claim)
-        if all(p not in overlap for p in ps):
-            return claim.num
+    good_claims = [
+        claim.id for claim in claims
+        if not claim_points(claim) & overlap
+    ]
+
+    assert len(good_claims) == 1
+    return good_claims[0]
 
 
 if __name__ == "__main__":
