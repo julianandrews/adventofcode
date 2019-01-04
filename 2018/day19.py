af37a97939590b0c3fc0c99aa4357b2a4ffa63c9
Julian Andrews <julianandrews@google.com>
2019-01-04 11:38:43 -0800

    Extract shared VM code

--- 2018/day19.py
+++ 2018/day19.py
@@ -1,57 +1,7 @@
 import collections
 import fileinput
 
-
-OpInfo = collections.namedtuple("OpInfo", ["op", "a_reg", "b_reg"])
-
-
-class VirtualMachine:
-    OPERATIONS = {
-        "addr": OpInfo(int.__add__, True, True),
-        "addi": OpInfo(int.__add__, True, False),
-        "mulr": OpInfo(int.__mul__, True, True),
-        "muli": OpInfo(int.__mul__, True, False),
-        "banr": OpInfo(int.__and__, True, True),
-        "bani": OpInfo(int.__and__, True, False),
-        "borr": OpInfo(int.__or__, True, True),
-        "bori": OpInfo(int.__or__, True, False),
-        "setr": OpInfo(lambda a, b: a, True, False),
-        "seti": OpInfo(lambda a, b: a, False, False),
-        "gtir": OpInfo(lambda a, b: 1 if a > b else 0, False, True),
-        "gtri": OpInfo(lambda a, b: 1 if a > b else 0, True, False),
-        "gtrr": OpInfo(lambda a, b: 1 if a > b else 0, True, True),
-        "eqir": OpInfo(lambda a, b: 1 if a == b else 0, False, True),
-        "eqri": OpInfo(lambda a, b: 1 if a == b else 0, True, False),
-        "eqrr": OpInfo(lambda a, b: 1 if a == b else 0, True, True),
-    }
-
-    def __init__(self, ip_register, registers=None):
-        self.ip_register = ip_register
-        self.registers = registers[:] if registers is not None else [0, 0, 0, 0, 0, 0]
-
-    def operate(self, op_name, a, b, c):
-        op_info = self.OPERATIONS[op_name]
-        a_value = self.registers[a] if op_info.a_reg else a
-        b_value = self.registers[b] if op_info.b_reg else b
-
-        self.registers[c] = op_info.op(a_value, b_value)
-
-    def run_program(self, program):
-        ip = self.registers[self.ip_register]
-        while ip < len(program):
-            instruction = program[ip]
-            self.registers[self.ip_register] = ip
-            original_registers = self.registers[:]
-            original_ip = ip
-            self.operate(*instruction)
-            ip = self.registers[self.ip_register]
-            ip += 1
-            # print("ip=%s %s %s %s" % (original_ip, original_registers, " ".join(map(str, instruction)), self.registers))
-
-
-def parse_instruction(line):
-    op_name, a, b, c = line.split()
-    return (op_name, int(a), int(b), int(c))
+from utils.vm import VirtualMachine
 
 
 def p1(ip_register, program):
@@ -106,5 +56,5 @@ For my input r0 = 0, r4 = 10551394
 if __name__ == "__main__":
     lines = [line.strip() for line in fileinput.input()]
     ip_register = int(lines[0].split()[1])
-    program = [parse_instruction(line) for line in lines[1:]]
+    program = [VirtualMachine.parse_instruction(line) for line in lines[1:]]
     print("Part 1: %s" % p1(ip_register, program))
