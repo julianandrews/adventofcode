c8e68138c0aba4e3bd37b58f54ffc62a60238802
Julian Andrews <jandrews@fusionbox.com>
2019-01-27 21:15:26 -0800

    Finish 2015 day 20 (and all of 2015 is done!)

--- 2015/day20.py
+++ 2015/day20.py
@@ -1,55 +1,37 @@
-import collections
 import fileinput
-import functools
 import math
 
 
-def primes_to(n):
-    is_prime = [False] * 2 + [True] * (n - 1)
-    for i in range(int(n**0.5 + 1.5)):
-        if is_prime[i]:
-            for j in range(i*i, n+1, i):
-                is_prime[j] = False
-
-    return [i for i, prime in enumerate(is_prime) if prime]
-
-
-def prime_factor_counts(primes, n):
-    counts = collections.defaultdict(int)
-    for p in primes:
-        while n % p == 0:
-            counts[p] += 1
-            n //= p
-        if n == 1:
-            return counts
-
-
-def sum_of_divisors(primes, n):
-    counts = prime_factor_counts(primes, n)
-
-    return functools.reduce(
-        int.__mul__,
-        ((p**(n + 1) - 1) // (p - 1) for p, n in counts.items()),
-        1
-    )
-
-
 def part1(n):
-    n = n // 10
-    primes = primes_to(n)
+    target = math.ceil(n / 10.0)
+    counts = [0] * (target + 1)
 
-    for house in range(1, n + 1):
-        if sum_of_divisors(primes, house) >= n:
-            return house
+    for elf in range(1, target + 1):
+        value = elf
+        while value < target + 1:
+            counts[value] += elf
+            value += elf
+    for i, count in enumerate(counts):
+        if count > target:
+            return i
 
 
 def part2(n):
-    # target = math.ceil(n / 11.0)
-    pass
+    target = math.ceil(n / 11.0)
+    counts = [0] * (target + 1)
+    for elf in range(1, target + 1):
+        for multiplier in range(1, 51):
+            value = multiplier * elf
+            if value > target + 1:
+                break
+            counts[value] += elf
+    for i, count in enumerate(counts):
+        if count > target:
+            return i
 
 
 if __name__ == "__main__":
     n = int(next(fileinput.input()).strip())
 
-    # print("Part 1: %s" % part1(n))
+    print("Part 1: %s" % part1(n))
     print("Part 2: %s" % part2(n))
