f88030e3444fc234d1b0351710f009b6c1c0eef2
Julian Andrews <jandrews@fusionbox.com>
2019-01-29 06:44:10 -0800

    2017 setup

---
+++ 2017/template.py
@@ -0,0 +1,13 @@
+from utils import read_data, get_lines,
+
+
+def run_tests():
+    pass
+
+
+if __name__ == "__main__":
+    data = read_data(0)
+    run_tests()
+    print("All tests passed")
+    print("Part 1: {}".format(None))
+    print("Part 2: {}".format(None))
---
+++ 2017/utils/__init__.py
@@ -0,0 +1,7 @@
+def read_data(n):
+    with open('inputs/d{:02d}/input.txt'.format(n)) as f:
+        return f.read()
+
+
+def get_lines(data):
+    return [line.strip() for line in data.strip().split('\n')]
