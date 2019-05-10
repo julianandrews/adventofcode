6116f39c85eec8c5d63b64d144a2080173c19383
Julian Andrews <jandrews@fusionbox.com>
2019-05-09 19:30:16 -0700

    2017 day 23

--- 2017/utils/primes.py
+++ 2017/utils/primes.py
@@ -1,3 +1,12 @@
+def is_prime(n):
+    d = 2
+    while d * d <= n:
+        if n % d == 0:
+            return False
+        d += 1
+    return n > 1
+
+
 def prime_factors(n):
     i = 2
     while i <= n:
@@ -5,3 +14,14 @@ def prime_factors(n):
             yield i
             n //= i
         i += 1
+
+
+def primes_upto(limit):
+    is_prime = [False] * 2 + [True] * (limit - 1)
+    for n in range(int(limit**0.5 + 1.5)):
+        if is_prime[n]:
+            for i in range(n * n, limit + 1, n):
+                is_prime[i] = False
+    for i in range(limit + 1):
+        if is_prime[i]:
+            yield i
