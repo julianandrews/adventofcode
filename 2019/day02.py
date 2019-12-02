ba74edb1b0921b4522ff0c645a709e87bbff0a3b
Julian Andrews <jandrews@fusionbox.com>
2019-12-02 08:39:15 -0800

    Refactor 2019 intcode VM
    This should make life easier in the (inevitable) follow up problems.

--- 2019/day02.py
+++ 2019/day02.py
@@ -2,28 +2,32 @@ import utils
 from utils.intcode import VM
 
 
-def run_with_inputs(program, noun, verb):
-    vm = VM.with_inputs(program[:], noun, verb)
+def run_with_inputs(program, noun=None, verb=None):
+    vm = VM(program[:], noun, verb)
     vm.run()
 
-    return vm.output
+    return vm
 
 
 def p1(program):
-    return run_with_inputs(program, 12, 2)
+    return run_with_inputs(program, 12, 2).output
 
 
 def p2(program):
     for a in range(99):
         for b in range(99):
-            if run_with_inputs(program, a, b) == 19690720:
+            if run_with_inputs(program, a, b).output == 19690720:
                 return 100 * a + b
 
 
 if __name__ == "__main__":
+    # import logging; logging.basicConfig(level=logging.DEBUG)
     data = utils.read_data(2)
     program = [int(x) for x in data.strip().split(',')]
-    test_program = [int(x) for x in "1,9,10,3,2,3,11,0,99,30,40,50".split(",")]
-    assert run_with_inputs(test_program, 9, 10) == 3500
+    assert run_with_inputs([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50], 9, 10).output == 3500
+    assert run_with_inputs([1, 0, 0, 0, 99]).memory == [2, 0, 0, 0, 99]
+    assert run_with_inputs([2, 3, 0, 3, 99]).memory == [2, 3, 0, 6, 99]
+    assert run_with_inputs([2, 4, 4, 5, 99, 0]).memory == [2, 4, 4, 5, 99, 9801]
+    assert run_with_inputs([1, 1, 1, 4, 99, 5, 6, 0, 99]).memory == [30, 1, 1, 4, 2, 5, 6, 0, 99]
     print("Part 1: %d" % p1(program))
     print("Part 2: %d" % p2(program))
--- 2019/utils/intcode.py
+++ 2019/utils/intcode.py
@@ -1,4 +1,5 @@
 import enum
+import logging
 
 
 class Op(enum.Enum):
@@ -7,31 +8,36 @@ class Op(enum.Enum):
     HALT = 99
 
     @property
-    def parameters(self):
-        if self == Op.HALT:
-            return 0
-        elif self in {Op.ADD, Op.MULTIPLY}:
+    def num_parameters(self):
+        if self.is_binary:
             return 3
+        elif self == Op.HALT:
+            return 0
+        else:
+            raise RuntimeError(f"Unexpected Operation {self}")
+
+    @property
+    def is_binary(self):
+        return self in {Op.ADD, Op.MULTIPLY}
 
 
 class VM:
-    def __init__(self, memory):
+    logger = logging.getLogger(__name__)
+
+    def __init__(self, memory, noun=None, verb=None):
+        self.logger.debug(f"Initializing VM")
         self.memory = memory
         self.ip = 0
-        self.halted = False
-
-    @classmethod
-    def with_inputs(cls, memory, noun, verb):
-        vm = VM(memory)
-        vm.set_inputs(noun, verb)
-        return vm
+        if noun is not None:
+            self.noun = noun
+        if verb is not None:
+            self.verb = verb
 
     def step(self):
         op = Op(self.memory[self.ip])
-        params = self.memory[self.ip + 1:self.ip + op.parameters + 1]
-        if op == Op.HALT:
-            self.halted = True
-        else:
+        params = self.memory[self.ip + 1:self.ip + op.num_parameters + 1]
+        self.logger.debug(f"Executing {op} with {params}")
+        if op.is_binary:
             a = self.memory[params[0]]
             b = self.memory[params[1]]
             target = params[2]
@@ -39,16 +45,43 @@ class VM:
                 self.memory[target] = a + b
             elif op == Op.MULTIPLY:
                 self.memory[target] = a * b
+        elif op == Op.HALT:
+            pass
+        else:
+            raise RuntimeError("Unexpected Operation {op}")
 
-        self.ip += 1 + op.parameters
+        self.ip += 1 + op.num_parameters
+        return op, params
 
-    def set_inputs(self, noun, verb):
-        self.memory[1] = noun
-        self.memory[2] = verb
+    def set_verb(self, verb):
+        self.logger.debug(f"Setting verb to {verb}")
+        if verb is not None:
+            self.memory[2] = verb
 
     def run(self):
-        while not self.halted:
-            self.step()
+        self.logger.info(f"Starting program with inputs {self.noun}, {self.verb}")
+        while True:
+            op, _ = self.step()
+            if op == Op.HALT:
+                return
+
+    @property
+    def noun(self):
+        return self.memory[1]
+
+    @noun.setter
+    def noun(self, value):
+        self.logger.debug(f"Setting noun to {value}")
+        self.memory[1] = value
+
+    @property
+    def verb(self):
+        return self.memory[2]
+
+    @verb.setter
+    def verb(self, value):
+        self.logger.debug(f"Setting verb to {value}")
+        self.memory[2] = value
 
     @property
     def output(self):
