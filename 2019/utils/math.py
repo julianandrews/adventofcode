32f9be33e49666ea676d3e213077d04d7a6ed967
Julian Andrews <jandrews@fusionbox.com>
2020-01-03 10:30:45 -0800

    2019 day 22

--- 2019/utils/math.py
+++ 2019/utils/math.py
@@ -52,3 +52,21 @@ class Quadratic:
                 ]
 
         return []
+
+
+def extended_gcd(a, b):
+    """return (g, x, y) such that a*x + b*y = g = gcd(a, b)"""
+    x0, x1, y0, y1 = 0, 1, 1, 0
+    while a != 0:
+        (q, a), b = divmod(b, a), a
+        y0, y1 = y1, y0 - q * y1
+        x0, x1 = x1, x0 - q * x1
+    return b, x0, y0
+
+
+def modinverse(a, b):
+    """return x such that (x * a) % b == 1"""
+    g, x, _ = extended_gcd(a, b)
+    if g != 1:
+        raise ValueError('gcd(a, b) != 1')
+    return x % b
