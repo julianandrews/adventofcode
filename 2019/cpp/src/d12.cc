e7ad046d4092fe0e3fffb1c63dbb08b9ce408ff3
Julian Andrews <jandrews@fusionbox.com>
2020-01-11 11:37:46 -0800

    2019 day 12 in c++

---
+++ 2019/cpp/src/d12.cc
@@ -0,0 +1,130 @@
+#include <numeric>
+#include <vector>
+
+#include "point.h"
+#include "utils.h"
+
+typedef ::aoc::point::Point<int, 3> Coords;
+
+Coords parse_position(std::string s) {
+  auto parts = aoc::utils::split(s.substr(1, s.length() - 2), ',');
+  int x = std::stoi(aoc::utils::trim(parts[0]).substr(2));
+  int y = std::stoi(aoc::utils::trim(parts[1]).substr(2));
+  int z = std::stoi(aoc::utils::trim(parts[2]).substr(2));
+
+  return {x, y, z};
+}
+
+class Moon {
+  Coords position_;
+  Coords velocity_ = Coords({0, 0, 0});
+
+  void update_velocity(const std::vector<Moon> &moons) {
+    for (const auto &moon : moons) {
+      for (int i = 0; i < 3; ++i) {
+        int offset = position_[i] - moon.position_[i];
+        if (offset < 0) {
+          velocity_[i] += 1;
+        } else if (offset > 0) {
+          velocity_[i] -= 1;
+        }
+      }
+    }
+  }
+
+  void update_position() {
+    for (int i = 0; i < 3; ++i) {
+      position_[i] += velocity_[i];
+    }
+  }
+
+  friend class PlanetarySystem;
+
+public:
+  Moon(Coords position) : position_(position) {}
+
+  int energy() const {
+    int p = 0;
+    int v = 0;
+    for (int i = 0; i < 3; ++i) {
+      p += std::abs(position_[i]);
+      v += std::abs(velocity_[i]);
+    }
+    return p * v;
+  }
+};
+
+class PlanetarySystem {
+public:
+  std::vector<Moon> moons_;
+
+  PlanetarySystem(std::vector<Moon> moons) : moons_(moons) {}
+
+  void step() {
+    for (auto &moon : moons_) {
+      moon.update_velocity(moons_);
+    }
+    for (auto &moon : moons_) {
+      moon.update_position();
+    }
+  }
+
+  int energy() const {
+    return std::accumulate(
+        moons_.begin(), moons_.end(), 0,
+        [](int total, const Moon &moon) { return total + moon.energy(); });
+  }
+
+  std::array<long, 3> dimension_periods() {
+    std::vector<Coords> initial_positions;
+    for (const auto &moon : moons_) {
+      initial_positions.push_back(moon.position_);
+    }
+    std::array<long, 3> periods = {0, 0, 0};
+    int steps = 0;
+    while (periods[0] == 0 || periods[1] == 0 || periods[2] == 0) {
+      step();
+      ++steps;
+      for (int i = 0; i < 3; ++i) {
+        if (periods.at(i) == 0) {
+          auto is_cycle = [&](int m) {
+            const auto &moon = moons_.at(m);
+            return moon.position_[i] == initial_positions[m][i] &&
+                   moon.velocity_[i] == 0;
+          };
+          std::vector<int> moon_indices;
+          for (int m = 0; m < moons_.size(); ++m)
+            moon_indices.push_back(m);
+          if (std::all_of(moon_indices.begin(), moon_indices.end(), is_cycle)) {
+            periods[i] = steps;
+          }
+        }
+      }
+    }
+    return periods;
+  }
+};
+
+int p1(PlanetarySystem system) {
+  for (int i = 0; i < 1000; ++i) {
+    system.step();
+  }
+  return system.energy();
+}
+
+long p2(PlanetarySystem system) {
+  auto periods = system.dimension_periods();
+  return std::accumulate(periods.begin(), periods.end(), 1ll,
+                         [](auto a, auto b) { return std::lcm(a, b); });
+}
+
+int main() {
+  std::vector<std::string> lines = aoc::utils::getlines();
+  std::vector<Moon> moons;
+  for (const auto &line : lines) {
+    moons.push_back(parse_position(line));
+  }
+  PlanetarySystem system(std::move(moons));
+  std::cout << "Part 1: " << p1(system) << std::endl;
+  std::cout << "Part 2: " << p2(system) << std::endl;
+}
--- 2019/cpp/src/lib/point.h
+++ 2019/cpp/src/lib/point.h
@@ -6,16 +6,19 @@
 namespace aoc {
 namespace point {
 
-template <class T, unsigned size> struct Point {
+template <class T, std::size_t size> struct Point {
   T values[size];
 
   bool operator==(const Point &other) const {
-    for (unsigned i = 0; i < size; ++i) {
+    for (std::size_t i = 0; i < size; ++i) {
       if (values[i] != other.values[i])
         return false;
     }
     return true;
   }
+
+  const T &operator[](int index) const { return values[index]; }
+  T &operator[](int index) { return values[index]; }
 };
 
 } // namespace point
@@ -23,10 +26,10 @@ template <class T, unsigned size> struct Point {
 
 namespace std {
 
-template <class T, unsigned size> struct hash<aoc::point::Point<T, size>> {
+template <class T, std::size_t size> struct hash<aoc::point::Point<T, size>> {
   std::size_t operator()(const aoc::point::Point<T, size> &p) const {
     std::size_t hash_value = 0;
-    for (unsigned i = 0; i < size; ++i) {
+    for (std::size_t i = 0; i < size; ++i) {
       hash_value ^= std::hash<T>()(p.values[i]);
     }
     return hash_value;
