julianandrews/adventofcode

commit b308a10194eda9597512d55342d7ad8e386c1cde

Rework c++ BFS to use iterator

2019/cpp/src/d15.cc
@@ -12,7 +12,6 @@
 #include "strings.h"
 
 using ::aoc::direction::Direction;
-using ::aoc::graphs::BFSTraversal;
 
 typedef ::aoc::point::Point<long long, 2> Coords;
 
@@ -167,11 +166,10 @@ public:
 };
 
 int p1(const Robot &robot) {
-  auto traversal = BFSTraversal<Coords, NeighborIterator>(robot, {0, 0});
-  while (traversal.hasnext()) {
-    auto node = traversal.next();
-    if (robot.status_at(node->value) == StatusCode::FOUND_OXYGEN) {
-      return node->depth;
+  auto bfs = aoc::graphs::BFS<Coords, NeighborIterator>(robot, {0, 0});
+  for (const auto &node : bfs) {
+    if (robot.status_at(node.value) == StatusCode::FOUND_OXYGEN) {
+      return node.depth;
     }
   }
   return 0;
@@ -180,22 +178,19 @@ int p1(const Robot &robot) {
 int p2(const Robot &robot) {
   // Find the oxygen.
   Coords start_position;
-  auto initial_traversal =
-      BFSTraversal<Coords, NeighborIterator>(robot, {0, 0});
-  while (initial_traversal.hasnext()) {
-    auto node = initial_traversal.next();
-    if (robot.status_at(node->value) == StatusCode::FOUND_OXYGEN) {
-      start_position = node->value;
+  auto initial_bfs = aoc::graphs::BFS<Coords, NeighborIterator>(robot, {0, 0});
+  for (const auto &node : initial_bfs) {
+    if (robot.status_at(node.value) == StatusCode::FOUND_OXYGEN) {
+      start_position = node.value;
       break;
     }
   }
 
   // Find the farthest point from it.
   int depth;
-  auto traversal =
-      BFSTraversal<Coords, NeighborIterator>(robot, start_position);
-  while (traversal.hasnext()) {
-    depth = traversal.next()->depth;
+  auto bfs = aoc::graphs::BFS<Coords, NeighborIterator>(robot, {0, 0});
+  for (const auto &node : bfs) {
+    depth = node.depth;
   }
   return depth;
 }

2019/cpp/src/d06.cc
@@ -8,8 +8,7 @@
 #include "strings.h"
 
 typedef ::std::unordered_set<std::string>::const_iterator NeighborIterator;
-typedef ::aoc::graphs::BFSTraversal<std::string, NeighborIterator>
-    OrbitGraphTraversal;
+typedef ::aoc::graphs::BFS<std::string, NeighborIterator> OrbitGraphBFS;
 
 class OrbitGraph : public aoc::graphs::Graph<std::string, NeighborIterator> {
   std::unordered_map<std::string, std::unordered_set<std::string>> orbits_;
@@ -36,21 +35,20 @@ public:
 };
 
 int p1(const OrbitGraph &orbit_graph) {
-  auto traversal = OrbitGraphTraversal(orbit_graph, "COM");
+  auto bfs = OrbitGraphBFS(orbit_graph, "COM");
   int total = 0;
-  while (traversal.hasnext()) {
-    total += traversal.next()->depth;
+  for (const auto &node : bfs) {
+    total += node.depth;
   }
 
   return total;
 }
 
 int p2(const OrbitGraph &orbit_graph) {
-  auto traversal = OrbitGraphTraversal(orbit_graph, "YOU");
-  while (traversal.hasnext()) {
-    auto node = traversal.next();
-    if (node->value == "SAN") {
-      return node->depth - 2;
+  auto bfs = OrbitGraphBFS(orbit_graph, "YOU");
+  for (const auto &node : bfs) {
+    if (node.value == "SAN") {
+      return node.depth - 2;
     }
   }
 

2019/cpp/src/lib/graphs.h
@@ -2,6 +2,7 @@
 #define AOC_GRAPHS_H
 
 #include <iostream>
+#include <iterator>
 #include <memory>
 #include <queue>
 #include <unordered_map>
@@ -30,28 +31,43 @@ template <class T> struct TraversalNode {
  * T must implement `operator==()` and must be hashable.
  * T is assumed to be cheap/fast to copy.
  */
-template <class T, class NeighborIterator> class Graph {
+template <class T, class NeighborIterator,
+          class NeighborIteratorEnd = NeighborIterator>
+class Graph {
 public:
   virtual NeighborIterator neighbors_begin(const T &value) const = 0;
 
-  virtual NeighborIterator neighbors_end(const T &value) const = 0;
+  virtual NeighborIteratorEnd neighbors_end(const T &value) const = 0;
 };
 
-template <class T, class NeighborIterator> class BFSTraversal {
-  const Graph<T, NeighborIterator> &graph_;
+template <class T, class NeighborIterator,
+          class NeighborIteratorEnd = NeighborIterator>
+class BFSIterator {
+  const Graph<T, NeighborIterator, NeighborIteratorEnd> &graph_;
   std::unordered_set<T> visited_;
   std::queue<std::shared_ptr<TraversalNode<T>>> to_visit_;
   int index_ = 0;
 
 public:
-  BFSTraversal(const Graph<T, NeighborIterator> &graph, const T start)
+  using iterator_category = std::input_iterator_tag;
+  using value_type = TraversalNode<T>;
+  using reference = TraversalNode<T> &;
+  using pointer = std::shared_ptr<TraversalNode<T>>;
+  using diference_type = void;
+
+  BFSIterator(const Graph<T, NeighborIterator, NeighborIteratorEnd> &graph,
+              const T start)
       : graph_(graph) {
     to_visit_.push(std::make_shared<TraversalNode<T>>(start, 0, 0));
   }
 
-  bool hasnext() const { return !to_visit_.empty(); }
+  BFSIterator(const BFSIterator &other)
+      : graph_(other.graph_), visited_(other.visited_),
+        to_visit_(other.to_visit_), index_(other.index_) {}
 
-  const std::shared_ptr<TraversalNode<T>> next() {
+  bool empty() const { return to_visit_.empty(); }
+
+  BFSIterator<T, NeighborIterator, NeighborIteratorEnd> &operator++() {
     auto node = to_visit_.front();
     to_visit_.pop();
     for (auto neighbor = graph_.neighbors_begin(node->value);
@@ -63,19 +79,92 @@ public:
       }
       visited_.insert(node->value);
     }
-
-    return node;
+    return *this;
   }
+
+  TraversalNode<T> &operator*() { return *to_visit_.front(); }
 };
 
-template <class T, class NeighborIterator> class TopologicalTraversal {
-  const Graph<T, NeighborIterator> &graph_;
+struct BFSIteratorEnd {};
+
+template <class T, class NeighborIterator,
+          class NeighborIteratorEnd = NeighborIterator>
+bool operator==(
+    const BFSIterator<T, NeighborIterator, NeighborIteratorEnd> &lhs,
+    const BFSIterator<T, NeighborIterator, NeighborIteratorEnd> &rhs) {
+  return lhs.index_ == rhs.index_;
+}
+
+template <class T, class NeighborIterator,
+          class NeighborIteratorEnd = NeighborIterator>
+bool operator==(
+    const BFSIterator<T, NeighborIterator, NeighborIteratorEnd> &lhs,
+    const BFSIteratorEnd &rhs) {
+  return lhs.empty();
+}
+
+template <class T, class NeighborIterator,
+          class NeighborIteratorEnd = NeighborIterator>
+bool operator==(
+    const BFSIteratorEnd &lhs,
+    const BFSIterator<T, NeighborIterator, NeighborIteratorEnd> &rhs) {
+  return rhs.empty();
+}
+
+template <class T, class NeighborIterator,
+          class NeighborIteratorEnd = NeighborIterator>
+bool operator!=(
+    const BFSIterator<T, NeighborIterator, NeighborIteratorEnd> &lhs,
+    const BFSIteratorEnd &rhs) {
+  return !(lhs == rhs);
+}
+
+template <class T, class NeighborIterator,
+          class NeighborIteratorEnd = NeighborIterator>
+bool operator!=(
+    const BFSIteratorEnd &lhs,
+    const BFSIterator<T, NeighborIterator, NeighborIteratorEnd> &rhs) {
+  return !(lhs == rhs);
+}
+
+template <class T, class NeighborIterator,
+          class NeighborIteratorEnd = NeighborIterator>
+bool operator!=(
+    const BFSIterator<T, NeighborIterator, NeighborIteratorEnd> &lhs,
+    const BFSIterator<T, NeighborIterator, NeighborIteratorEnd> &rhs) {
+  return !(lhs == rhs);
+}
+
+template <class T, class NeighborIterator,
+          class NeighborIteratorEnd = NeighborIterator>
+class BFS {
+  const Graph<T, NeighborIterator, NeighborIteratorEnd> &graph_;
+  const T &start_;
+
+public:
+  BFS(const Graph<T, NeighborIterator, NeighborIteratorEnd> &graph,
+      const T &start)
+      : graph_(graph), start_(start) {}
+
+  BFSIterator<T, NeighborIterator, NeighborIteratorEnd> begin() {
+    return BFSIterator<T, NeighborIterator, NeighborIteratorEnd>(graph_,
+                                                                 start_);
+  }
+
+  BFSIteratorEnd end() { return BFSIteratorEnd(); }
+};
+
+template <class T, class NeighborIterator,
+          class NeighborIteratorEnd = NeighborIterator>
+class TopologicalTraversal {
+  const Graph<T, NeighborIterator, NeighborIteratorEnd> &graph_;
   std::unordered_map<T, int> indegrees_;
   std::vector<T> to_visit_;
 
 public:
-  TopologicalTraversal(const Graph<T, NeighborIterator> &graph,
-                       const std::unordered_set<T> &values)
+  TopologicalTraversal(
+      const Graph<T, NeighborIterator, NeighborIteratorEnd> &graph,
+      const std::unordered_set<T> &values)
       : graph_(graph) {
     for (const auto &value : values) {
       for (auto neighbor = graph_.neighbors_begin(value);

commit 48994ca548a78588abebf3793f672fa0746ba797

Fix typo bug introduced in b308a10

2019/cpp/src/d15.cc
@@ -140,7 +140,7 @@ int p2(const Robot &robot) {
 
   // Find the farthest point from it.
   int depth;
-  auto bfs = aoc::graphs::BFS<Coords, Neighbors>(robot, {0, 0});
+  auto bfs = aoc::graphs::BFS<Coords, Neighbors>(robot, start_position);
   for (const auto &node : bfs) {
     depth = node.depth;
   }