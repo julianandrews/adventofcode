6dc793419849a18559fbd07eb03221fc43e92c82
Julian Andrews <jandrews@fusionbox.com>
2020-02-19 10:43:40 -0800

    Improve c++ Point hash function
    Performance testing showed that the performance issue with day 20 was
    due to hash collisions. This new hash function makes day 20 run in
    ~1/1000th of the time it took before.

--- 2019/cpp/src/lib/point.h
+++ 2019/cpp/src/lib/point.h
@@ -24,6 +24,14 @@ template <class T, std::size_t size> struct Point {
   T &operator[](int index) { return values[index]; }
 };
 
+namespace details {
+
+std::size_t cantor_pairing(std::size_t a, std::size_t b) {
+  return (a + b) * (a + b + 1) / 2 + b;
+}
+
+} // namespace details
+
 } // namespace point
 } // namespace aoc
 
@@ -33,7 +41,8 @@ template <class T, std::size_t size> struct hash<aoc::point::Point<T, size>> {
   std::size_t operator()(const aoc::point::Point<T, size> &point) const {
     std::size_t hash_value = 0;
     for (std::size_t i = 0; i < size; ++i) {
-      hash_value ^= std::hash<T>()(point.values[i]);
+      hash_value = aoc::point::details::cantor_pairing(
+          hash_value, std::hash<T>()(point.values[i]));
     }
     return hash_value;
   }
