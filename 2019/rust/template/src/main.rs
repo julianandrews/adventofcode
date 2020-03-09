92bd72d9c7577b4a6be84c4a317b183ec746194f
Julian Andrews <jandrews@fusionbox.com>
2020-03-08 20:31:06 -0700

    Add Rust template for 2019

---
+++ 2019/rust/template/src/main.rs
@@ -0,0 +1,23 @@
+extern crate aoc;
+
+use aoc::aoc_error::AOCError;
+use std::io::{self, Read};
+
+type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
+
+fn part1() -> Result<u64> {
+    Err(AOCError::new("Unimplemented"))?
+}
+
+fn part2() -> Result<u64> {
+    Err(AOCError::new("Unimplemented"))?
+}
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    println!("Part 1: {}", part1()?);
+    println!("Part 2: {}", part2()?);
+    Ok(())
+}
