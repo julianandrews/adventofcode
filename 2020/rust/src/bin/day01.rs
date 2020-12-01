aa73c1eeb8fc9d89b8bbe100f6724feef0a75efd
Julian Andrews <jandrews@fusionbox.com>
2020-12-01 11:22:52 -0500

    2020 day 1

---
+++ 2020/rust/src/bin/day01.rs
@@ -0,0 +1,62 @@
+use aoc::aoc_error::AOCError;
+use aoc::utils::{get_input, parse_fields};
+use itertools::Itertools;
+use std::collections::HashSet;
+
+type Result<T> = ::std::result::Result<T, Box<dyn::std::error::Error>>;
+
+fn find_combination_with_sum(entries: &[u64], n: usize, sum: u64) -> Option<HashSet<u64>> {
+    for combination in entries.iter().combinations(n) {
+        if combination.iter().copied().sum::<u64>() == sum {
+            return Some(combination.into_iter().copied().collect());
+        }
+    }
+    None
+}
+
+fn part1(entries: &[u64]) -> Result<u64> {
+    let combination =
+        find_combination_with_sum(entries, 2, 2020).ok_or(AOCError::new("No pair found"))?;
+
+    Ok(combination.iter().product())
+}
+
+fn part2(entries: &[u64]) -> Result<u64> {
+    let combination =
+        find_combination_with_sum(entries, 3, 2020).ok_or(AOCError::new("No triple found"))?;
+
+    Ok(combination.iter().product())
+}
+
+fn main() -> Result<()> {
+    let input = get_input()?;
+    let entries = parse_fields(input.trim(), '\n')?;
+
+    println!("Part 1: {}", part1(&entries)?);
+    println!("Part 2: {}", part2(&entries)?);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn find_pair() {
+        let entries = vec![1721, 979, 366, 299, 675];
+        let expected: HashSet<_> = vec![1721, 299].into_iter().collect();
+
+        let result = find_combination_with_sum(&entries, 2, 2020);
+        assert_eq!(result, Some(expected));
+    }
+
+    #[test]
+    fn find_triple() {
+        let entries = vec![1721, 979, 366, 299, 675];
+        let expected: HashSet<_> = vec![979, 366, 675].into_iter().collect();
+
+        let result = find_combination_with_sum(&entries, 3, 2020);
+
+        assert_eq!(result, Some(expected));
+    }
+}
---
+++ 2020/rust/src/utils.rs
@@ -0,0 +1,31 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, Read};
+use std::result::Result;
+use std::str::FromStr;
+
+pub fn get_input() -> Result<String, Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    let filename = if args.len() == 2 && args[1] != "-" {
+        Some(args[1].clone())
+    } else {
+        None
+    };
+    let mut reader: Box<dyn io::Read> = match &filename {
+        Some(filename) => Box::new(io::BufReader::new(File::open(&filename)?)),
+        None => Box::new(io::stdin()),
+    };
+
+    let mut input = String::new();
+    reader.read_to_string(&mut input)?;
+
+    Ok(input)
+}
+
+// TODO: See if I can use `std::str::Pattern` instead of `char`
+pub fn parse_fields<T: FromStr>(
+    data: &str,
+    delimiter: char,
+) -> Result<Vec<T>, <T as FromStr>::Err> {
+    data.split(delimiter).map(&str::parse).collect()
+}
