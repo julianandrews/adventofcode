623d5a6e6cb036e0620714974f88fb931565d5c7
Julian Andrews <jandrews@fusionbox.com>
2020-12-02 10:39:22 -0500

    Reorganize day 1
    I think I want to do organize my code in 'reading' order. This feels
    cleaner and easier to understand.

--- 2020/rust/src/bin/day01.rs
+++ 2020/rust/src/bin/day01.rs
@@ -3,15 +3,15 @@ use aoc::utils::{get_input, parse_fields};
 use itertools::Itertools;
 use std::collections::HashSet;
 
-type Result<T> = ::std::result::Result<T, Box<dyn::std::error::Error>>;
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-fn find_combination_with_sum(entries: &[u64], n: usize, sum: u64) -> Option<HashSet<u64>> {
-    for combination in entries.iter().combinations(n) {
-        if combination.iter().copied().sum::<u64>() == sum {
-            return Some(combination.into_iter().copied().collect());
-        }
-    }
-    None
+fn main() -> Result<()> {
+    let input = get_input()?;
+    let entries = parse_fields(input.trim(), '\n')?;
+
+    println!("Part 1: {}", part1(&entries)?);
+    println!("Part 2: {}", part2(&entries)?);
+    Ok(())
 }
 
 fn part1(entries: &[u64]) -> Result<u64> {
@@ -28,13 +28,13 @@ fn part2(entries: &[u64]) -> Result<u64> {
     Ok(combination.iter().product())
 }
 
-fn main() -> Result<()> {
-    let input = get_input()?;
-    let entries = parse_fields(input.trim(), '\n')?;
-
-    println!("Part 1: {}", part1(&entries)?);
-    println!("Part 2: {}", part2(&entries)?);
-    Ok(())
+fn find_combination_with_sum(entries: &[u64], n: usize, sum: u64) -> Option<HashSet<u64>> {
+    for combination in entries.iter().combinations(n) {
+        if combination.iter().copied().sum::<u64>() == sum {
+            return Some(combination.into_iter().copied().collect());
+        }
+    }
+    None
 }
 
 #[cfg(test)]
