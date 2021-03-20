e8cba5676d18b10f0d08024693bf0ece7358f306
Julian Andrews <jandrews@fusionbox.com>
2021-03-20 10:03:32 -0400

    Simplify mod_mul
    This approach is so much more elegant, and cursory testing doesn't show
    a performance difference at a level I'm likely to care about.

--- 2020/rust/src/math.rs
+++ 2020/rust/src/math.rs
@@ -23,49 +23,7 @@ pub fn mod_add(a: u64, b: u64, m: u64) -> u64 {
     }
 }
 
-// Shamelessly stolen from
-// https://stackoverflow.com/questions/45918104/how-to-do-arithmetic-modulo-another-number-without-overflow
-pub fn mod_mul(mut a: u64, mut b: u64, m: u64) -> u64 {
-    let msb = 0x8000_0000_0000_0000;
-    let mut d = 0;
-    let mp2 = m >> 1;
-    a %= m;
-    b %= m;
-
-    if m & msb == 0 {
-        for _ in 0..64 {
-            d = if d > mp2 { (d << 1) - m } else { d << 1 };
-            if a & msb != 0 {
-                d += b;
-            }
-            if d >= m {
-                d -= m;
-            }
-            a <<= 1;
-        }
-        d
-    } else {
-        for _ in 0..64 {
-            d = if d > mp2 {
-                d.wrapping_shl(1).wrapping_sub(m)
-            } else {
-                // the case d == m && a == 0 is taken care of
-                // after the end of the loop
-                d << 1
-            };
-            if a & msb != 0 {
-                let (mut d1, overflow) = d.overflowing_add(b);
-                if overflow {
-                    d1 = d1.wrapping_sub(m);
-                }
-                d = if d1 >= m { d1 - m } else { d1 };
-            }
-            a <<= 1;
-        }
-        if d >= m {
-            d - m
-        } else {
-            d
-        }
-    }
+pub fn mod_mul(a: u64, b: u64, m: u64) -> u64 {
+    let (a, b, m) = (a as u128, b as u128, m as u128);
+    ((a * b) % m) as u64
 }
